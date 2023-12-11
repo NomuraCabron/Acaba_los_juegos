4a313f1524db012a36535a9515ab115e10521779
alexl <alexl@LAPTOP-K5F5FDFE>
2023-12-11 19:48:04 +0100

    Hemos puesto un scaner y dos variables

--- Acaba_los_juegos2.0/Proyecto-GitHub/src/proyecto/github/ProyectoGitHub.java
+++ Acaba_los_juegos2.0/Proyecto-GitHub/src/proyecto/github/ProyectoGitHub.java
@@ -12,7 +12,7 @@ public class ProyectoGitHub {
     
 
     public static void main(String[] args) {
-        System.out.println("Modificacio 1");
+        System.out.println("Modificacio 2");
     }
     
 }
